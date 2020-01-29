edef51907f5ac9e28800b3ca550a083653237444
jordan621k@gmail.com <jordan621k@gmail.com>
2020-01-29 00:03:49 -0500

    add youtube

--- src/component/Form.js
+++ src/component/Form.js
@@ -2,6 +2,8 @@ import React from 'react';
 import DropDownMenu from './DropDownMenu';
 import '../css/Form.css';
 
+const axios = require('axios');
+
 const countries = {
     'Brazil': 'BR',
     'Canada': 'CA',
@@ -35,6 +37,7 @@ class Form extends React.Component {
   handleSubmit(event) {
     console.log(this.state);
     event.preventDefault();
+    this.execute()
   }
 
   updateCountry(country) {
@@ -44,6 +47,30 @@ class Form extends React.Component {
   updateCategory(category) {
     this.setState({category: category});
   }
+
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  execute() { 
+  // Make a request for a user with a given ID
+  axios({
+    method: 'get',
+    url: 'https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&locale=zh-TW&maxResults=20&regionCode=TW&key=AIzaSyAN6WGBl3-zqdtlabrDV428Tn3zrlpeAW0',
+    responseType: 'json',
+    headers: {Authorization: 'Bearer AIzaSyAN6WGBl3-zqdtlabrDV428Tn3zrlpeAW0',
+              Accept: 'application/json'}
+    }).then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  }
+
+  
 
   render() {
     return (
