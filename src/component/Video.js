62378cf231d00d50dab37656981b8036bc8bcc18
miyang <miyang@expedia.com>
2020-02-03 18:13:11 -0800

    fix eslint error

--- src/component/Video.js
+++ src/component/Video.js
@@ -2,34 +2,35 @@ import React from 'react'
 import '../css/Video.css'
 
 class Video extends React.Component {
-  constructor(props) {
+
+  constructor (props) {
     super(props)
     this.openVideoInWindow = this.openVideoInWindow.bind(this)
   }
-    
-  openVideoInWindow() {
+
+  openVideoInWindow () {
     window.open(this.props.videoLink)
   }
 
-  render() {
+  render () {
     const { title, viewCount, likeCount, publishedAt, thumbnail, rank, channelTitle } = this.props
     return (
-        <div onMouseDown={this.openVideoInWindow}>
-            <div>
-                {rank}
-            </div>
-            <div>
-                <img src={thumbnail}/>
-            </div>
-            <div>
-                <b>TITLE: {title}  </b><b>CHANEL: {channelTitle}</b><br/>
-                <b>VIEWS: {viewCount}  </b>
-                <b>LIKES: {likeCount}  </b>
-                <b>DATE: {publishedAt}  </b>
-            </div>
+      <div onMouseDown={this.openVideoInWindow}>
+        <div>
+          {rank}
         </div>
-     )
+        <div>
+          <img src={thumbnail}/>
+        </div>
+        <div>
+          <b>TITLE: {title}  </b><b>CHANEL: {channelTitle}</b><br/>
+          <b>VIEWS: {viewCount}  </b>
+          <b>LIKES: {likeCount}  </b>
+          <b>DATE: {publishedAt}  </b>
+        </div>
+      </div>
+    )
   }
 }
 
-export default Video;
+export default Video
