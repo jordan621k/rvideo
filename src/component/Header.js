f05d0e27d4c414a47c1c88df1e31a596b1977fb4
miyang <miyang@expedia.com>
2020-03-13 22:26:26 -0700

    add logo to header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../css/Header.css'
+import logo from '../static/logo.png'
 
 class Header extends React.Component {
   render () {
     return (
       <header className="Header">
-        <h1>Viral Videos</h1>
+        <img src={logo} alt="logo" />
+        <h6>Trending Videos on Youtube</h6>
       </header>
     )
   }
