00eb26da3e33511355424b46b48859e69a1c174c
Jordan Yang <jordan621k@gmail.com>
2020-07-07 12:28:06 -0400

    move dropDown to header

--- src/component/App.js
+++ src/component/App.js
@@ -2,17 +2,13 @@ import React from 'react'
 import '../css/App.css'
 import Header from './Header'
 import Footer from './Footer'
-import Form from './Form'
 import VideoList from './VideoList'
 import { i18n, LocaleContext } from '../i18n/i18n'
-import { Router } from 'react-router'
-import { createBrowserHistory } from 'history'
 
 class App extends React.Component {
 
   constructor (props) {
     super(props)
-    this.history = createBrowserHistory()
     this.state = {
       locale: 'en_us',
       updateLocale: (localeCode) => {
@@ -28,9 +24,6 @@ class App extends React.Component {
       <LocaleContext.Provider value={this.state}>
         <Header/>
         <div className="App">
-          <Router history={this.history}>
-            <Form/>
-          </Router>
           <VideoList/>
         </div>
         <Footer/>
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,14 +1,31 @@
 import React from 'react'
-import '../css/Header.css'
+import '../css/Header.less'
 import logo from '../static/logo.png'
 import { i18n, LocaleContext } from '../i18n/i18n'
+import Form from './Form'
+import { Router } from 'react-router'
+import { createBrowserHistory } from 'history'
 
 class Header extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.history = createBrowserHistory()
+  }
+
   render () {
     return (
       <header className="Header">
-        <img src={logo} alt="logo" />
-        <h6>{i18n(this.context.locale).header}</h6>
+        <div className="logoAndText">
+          <img className="logoImage" src={logo} alt="logo" />
+        </div>
+        <div className="formBlock">
+          <h6>{i18n(this.context.locale).header}</h6>
+          <Router history={this.history}>
+            <Form/>
+          </Router>
+        </div>
+        <div className="block"></div>
       </header>
     )
   }
