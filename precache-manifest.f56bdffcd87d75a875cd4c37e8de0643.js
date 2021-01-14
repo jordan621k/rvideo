b6f2ace3eb7eaeead9fea66fd372ca716506cdd2
jordan621k@gmail.com <jordan621k@gmail.com>
2021-01-14 21:52:16 +0800

    Updates

---
+++ precache-manifest.f56bdffcd87d75a875cd4c37e8de0643.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02bbc78f4813417cfd72163273bd68f7",
+    "url": "/viralvideos/index.html"
+  },
+  {
+    "revision": "18aa56ff8f559ad78af4",
+    "url": "/viralvideos/static/css/main.678f695d.chunk.css"
+  },
+  {
+    "revision": "928c1b7731f430795d79",
+    "url": "/viralvideos/static/js/2.2983582f.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/viralvideos/static/js/2.2983582f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18aa56ff8f559ad78af4",
+    "url": "/viralvideos/static/js/main.ef59741f.chunk.js"
+  },
+  {
+    "revision": "0cf3f3a02eb196cdf8ec",
+    "url": "/viralvideos/static/js/runtime-main.e21072a1.js"
+  },
+  {
+    "revision": "1d2bec687badb4b3af438a7fee9c91ed",
+    "url": "/viralvideos/static/media/DropDownMenu.1d2bec68.less"
+  },
+  {
+    "revision": "c9d5c4698bebe1ce992a60bde88cfb08",
+    "url": "/viralvideos/static/media/Form.c9d5c469.less"
+  },
+  {
+    "revision": "1087e6f7ca5673ae67036a7408e48846",
+    "url": "/viralvideos/static/media/Header.1087e6f7.less"
+  },
+  {
+    "revision": "d55b97c61892cb782ee763c944d9e73a",
+    "url": "/viralvideos/static/media/Video.d55b97c6.less"
+  }
+]);
